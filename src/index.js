a3395a6c9581b1baab62f99460e97a77b64eeb37
extf8 <extfield8@gmail.com>
2023-12-07 12:39:52 +0200

    Added contents and images to menu page section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './styles/dark.css'
 import homePageLeft from './assets/img/homePageLeft.png';
 import homePageRight from './assets/img/homePageRight.png';
 
+import burger from './assets/img/menu/bisonBurger.png'
+import chicken from './assets/img/menu/chickenSaladMango.png'
+import lasagna from './assets/img/menu/lasagna.png'
+import pizza from './assets/img/menu/meatLoversPizza.png';
+import sauce from './assets/img/menu/pricklyPearPorkSauce.png'
+import tostada from './assets/img/menu/purpleCornTostada.png'
+
 // add classes for mobile navigation toggling
 var CSbody = document.querySelector('body');
 const CSnavbarMenu = document.querySelector('#cs-navigation');
