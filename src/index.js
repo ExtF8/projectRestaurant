69caa3135708365f0a732c6d41f8f2bbb2cbcae4
extf8 <extfield8@gmail.com>
2023-12-07 15:55:32 +0200

    Added event listener for navigation

--- src/index.js
+++ src/index.js
@@ -17,3 +17,44 @@ import tostada from './assets/img/menu/purpleCornTostada.png';
 
 darkModeToggler();
 mobileNavigationToggling();
+
+// Set globals
+let pageNames = ['home', 'menu', 'contacts'];
+let currentTab = pageNames[0];
+let content = document.getElementById('main-content');
+
+// Initial home page load
+// loadMain(currentTab, content, reviews, homeTitle, info);
+
+// Set click events for tab navigation
+pageNames.forEach(pageName => {
+  const button = document.getElementById(pageName);
+  button.addEventListener('click', () => {
+    currentTab = changeButtonCover(currentTab, button.id);
+    // button.classList.add('cs-active');
+
+    if (button.id == 'home') {
+        console.log('home')
+    //   loadMain(button.id, content, reviews, homeTitle, info);
+    } else if (button.id == 'menu') {
+        console.log('menu')
+
+    //   loadMain(button.id, content, menu, menuTitle);
+    } else {
+        console.log('contacts')
+
+    //   loadMain(button.id, content, contacts, contactTitle);
+    }
+  });
+})
+
+function changeButtonCover(oldTab, newTab) {
+    // let hide = document.getElementById(`cover${oldTab}`);
+    // let show = document.getElementById(`cover${newTab}`);
+    
+    button.classList.add('cs-active');
+    // show.classList.remove('cs-active');
+  
+    return newTab;
+  } 
+
