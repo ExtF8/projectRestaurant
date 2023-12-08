691e75819e37c0144d09155a7266d3d77eb89daf
extf8 <extfield8@gmail.com>
2023-12-08 18:07:52 +0200

    Refactored home module

--- src/modules/homePageLoader.js
+++ src/modules/homePageLoader.js
@@ -1,147 +1,147 @@
 import { homeContents } from './homeContents';
 
 // Home page content loader
-export default function homePageLoader(type) {
+export default function homePageLoader() {
     // Get main content element id
-    let mainContent = document.querySelector('#main-content');
-
-    // Create section
-    let section = document.createElement('section');
-    section.setAttribute('id', 'home-content-container');
-    mainContent.appendChild(section);
-
-    let homeContent = document.createElement('div');
-    homeContent.setAttribute('class', 'home-content');
-    section.appendChild(homeContent);
+    const mainContent = document.querySelector('#main-content');
+    const section = createSection('home-content-container');
+    const homeContent = createDiv('home-content');
+    const contentLeft = createContentLeft();
+    const contentRight = createContentRight();
 
     // Fill left section with content
-    let contentLeft = document.createElement('div');
-    contentLeft.setAttribute('class', 'home-content-left');
+    section.appendChild(homeContent);
     homeContent.appendChild(contentLeft);
-
-    let leftImage = document.createElement('picture');
-    leftImage.setAttribute('class', 'cs-picture');
-    leftImage.classList.add('cs-picture-left');
-    contentLeft.appendChild(leftImage);
-
-    let mobileSourceLeft = document.createElement('source');
-    let tabletSourceLeft = document.createElement('source');
-    let imgLeft = document.createElement('img');
-
-    leftImage.appendChild(mobileSourceLeft);
-    leftImage.appendChild(tabletSourceLeft);
-    leftImage.appendChild(imgLeft);
-
-    setAttributes(mobileSourceLeft, homeContents[0].imageLeft.mobileImage);
-    setAttributes(tabletSourceLeft, homeContents[0].imageLeft.tabletImage);
-    setAttributes(imgLeft, homeContents[0].imageLeft.img);
-
-    // Element under left image
-    let list = document.createElement('ul');
-
-    contentLeft.appendChild(list);
-    list.setAttribute('class', 'cs-stats');
-
-    (function firstStatContainer() {
-        let listItem = document.createElement('li');
-        listItem.setAttribute('class', 'cs-stat');
-
-        let firstSpan = document.createElement('span');
-        let secondSpan = document.createElement('span');
-
-        let firstStat = listItem;
-
-        firstStat.appendChild(firstSpan);
-        firstSpan.setAttribute('class', 'cs-number');
-        firstSpan.innerText = homeContents[0].stats.firstStat.num;
-
-        firstStat.appendChild(secondSpan);
-        secondSpan.setAttribute('class', 'cs-desc');
-        secondSpan.innerText = homeContents[0].stats.firstStat.text;
-        list.appendChild(listItem);
-
-        return firstStatContainer;
-    })();
-
-    (function secondStatContainer() {
-        let listItem = document.createElement('li');
-        listItem.setAttribute('class', 'cs-stat');
-
-        let firstSpan = document.createElement('span');
-        let secondSpan = document.createElement('span');
-
-        list.appendChild(listItem);
-        let secondStat = listItem;
-
-        secondStat.appendChild(firstSpan);
-        firstSpan.setAttribute('class', 'cs-number');
-        firstSpan.innerText = homeContents[0].stats.secondStat.num;
-
-        secondStat.appendChild(secondSpan);
-        secondSpan.setAttribute('class', 'cs-desc');
-        secondSpan.innerText = homeContents[0].stats.secondStat.text;
-
-        return secondStatContainer;
-    })();
-
-    (function thirdStatContainer() {
-        let listItem = document.createElement('li');
-        listItem.setAttribute('class', 'cs-stat');
-        let firstSpan = document.createElement('span');
-        let secondSpan = document.createElement('span');
-
-        list.appendChild(listItem);
-        let thirdStat = listItem;
-
-        thirdStat.appendChild(firstSpan);
-        firstSpan.setAttribute('class', 'cs-number');
-        firstSpan.innerText = homeContents[0].stats.thirdStat.num;
-
-        thirdStat.appendChild(secondSpan);
-        secondSpan.setAttribute('class', 'cs-desc');
-        secondSpan.innerText = homeContents[0].stats.thirdStat.text;
-
-        return thirdStatContainer;
-    })();
-
-    // Fill right section with content
-    let contentRight = document.createElement('div');
-    contentRight.setAttribute('class', 'home-content-right');
     homeContent.appendChild(contentRight);
-
-    let topper = document.createElement('span');
-    topper.setAttribute('class', 'cs-topper');
-    topper.innerText = homeContents[0].content.topper;
-    contentRight.appendChild(topper);
-
-    let h2 = document.createElement('h2');
-    h2.classList.add('cs-title');
-    h2.textContent = homeContents[0].content.title;
-    contentRight.appendChild(h2);
-
-    let paragraph = document.createElement('p');
-    paragraph.classList.add('cs-text');
-    paragraph.textContent = homeContents[0].content.paragraph;
-    contentRight.appendChild(paragraph);
-
-    let rightImage = document.createElement('picture');
-    rightImage.setAttribute('class', 'cs-picture');
-    rightImage.classList.add('cs-picture-right');
-    contentRight.appendChild(rightImage);
-
-    let mobileSourceRight = document.createElement('source');
-    let tabletSourceRight = document.createElement('source');
-    let imgRight = document.createElement('img');
-    rightImage.appendChild(mobileSourceRight);
-    rightImage.appendChild(tabletSourceRight);
-    rightImage.appendChild(imgRight);
-
-    setAttributes(mobileSourceRight, homeContents[0].imageRight.mobileImage);
-    setAttributes(tabletSourceRight, homeContents[0].imageRight.tabletImage);
-    setAttributes(imgRight, homeContents[0].imageRight.img);
+    mainContent.appendChild(section);
 }
 
-// helper function for setting attributes
+// Create section
+function createSection(id) {
+    const section = document.createElement('section');
+    section.setAttribute('id', id);
+    return section;
+}
+
+function createDiv(className) {
+    const div = document.createElement('div');
+    div.setAttribute('class', className);
+    return div;
+}
+
+function createContentLeft() {
+    const contentLeft = createDiv('home-content-left');
+    const leftImage = createImage(
+        'cs-picture cs-picture-left',
+        homeContents[0].imageLeft
+    );
+    const statsList = createStatsList();
+
+    contentLeft.appendChild(leftImage);
+    contentLeft.appendChild(statsList);
+
+    return contentLeft;
+}
+
+// Create Image
+function createImage(classNames, imageSources) {
+    const picture = document.createElement('picture');
+    picture.setAttribute('class', classNames);
+
+    Object.keys(imageSources).forEach((key) => {
+        if (key === 'img') {
+            const img = document.createElement('img');
+            setAttributes(img, imageSources[key]);
+            picture.appendChild(img);
+        } else {
+            const source = document.createElement('source');
+            setAttributes(source, imageSources[key]);
+            picture.appendChild(source);
+        }
+    });
+
+    return picture;
+}
+
+// Create stats list under left image
+function createStatsList() {
+    const list = document.createElement('ul');
+    list.setAttribute('class', 'cs-stats');
+
+    for (let key in homeContents[0].stats) {
+        if (homeContents[0].stats.hasOwnProperty(key)) {
+            list.appendChild(createStatItem(homeContents[0].stats[key]));
+        }
+    }
+
+    return list;
+}
+
+function createStatItem(stat) {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class', 'cs-stat');
+
+    const numberSpan = createSpan('cs-number', stat.num);
+    const textSpan = createSpan('cs-desc', stat.text);
+
+    listItem.appendChild(numberSpan);
+    listItem.appendChild(textSpan);
+
+    return listItem;
+}
+
+function createSpan(className, text) {
+    const span = document.createElement('span');
+    span.setAttribute('class', className);
+    span.innerText = text;
+
+    return span;
+}
+
+// Fill right section with content
+function createContentRight() {
+    const contentRight = createDiv('home-content-right');
+    const topper = createTopper('cs-topper');
+    const title = createTitle('cs-title');
+    const paragraph = createParagraph('cs-text');
+    const rightImage = createImage(
+        'cs-picture cs-picture-right',
+        homeContents[0].imageRight
+    );
+
+    contentRight.appendChild(topper);
+    contentRight.appendChild(title);
+    contentRight.appendChild(paragraph);
+    contentRight.appendChild(rightImage);
+
+    return contentRight;
+}
+
+function createTopper(className) {
+    const topper = document.createElement('span');
+    topper.setAttribute('class', className);
+    topper.innerText = homeContents[0].content.topper;
+
+    return topper;
+}
+
+function createTitle(className) {
+    const title = document.createElement('h2');
+    title.setAttribute('class', className);
+    title.textContent = homeContents[0].content.title;
+
+    return title;
+}
+
+function createParagraph(className) {
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', className);
+    paragraph.textContent = homeContents[0].content.paragraph;
+
+    return paragraph;
+}
+
+// helper function for setting multiple attributes
 function setAttributes(element, attributes) {
     Object.keys(attributes).forEach((attr) => {
         element.setAttribute(attr, attributes[attr]);
