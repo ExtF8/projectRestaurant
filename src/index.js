31043ba6b0c8394f8aeae744eebe1a3de19441ac
extf8 <extfield8@gmail.com>
2023-12-08 10:03:45 +0200

    Added page switching

--- src/index.js
+++ src/index.js
@@ -21,40 +21,38 @@ mobileNavigationToggling();
 // Set globals
 let pageNames = ['home', 'menu', 'contacts'];
 let currentTab = pageNames[0];
-let content = document.getElementById('main-content');
+let mainContent = document.getElementById('#main-content');
 
 // Initial home page load
 // loadMain(currentTab, content, reviews, homeTitle, info);
 
 // Set click events for tab navigation
-pageNames.forEach(pageName => {
-  const button = document.getElementById(pageName);
-  button.addEventListener('click', () => {
-    currentTab = changeButtonCover(currentTab, button.id);
-    // button.classList.add('cs-active');
+pageNames.forEach((pageName) => {
+    const button = document.getElementById(pageName);
+    button.addEventListener('click', () => {
+        currentTab = changeButtonCover(currentTab, button.id);
 
-    if (button.id == 'home') {
-        console.log('home')
-    //   loadMain(button.id, content, reviews, homeTitle, info);
-    } else if (button.id == 'menu') {
-        console.log('menu')
+        if (button.id == 'home') {
+            console.log('home');
+            //   loadMain(button.id, content, reviews, homeTitle, info);
+        } else if (button.id == 'menu') {
+            console.log('menu');
 
-    //   loadMain(button.id, content, menu, menuTitle);
-    } else {
-        console.log('contacts')
+            //   loadMain(button.id, content, menu, menuTitle);
+        } else {
+            console.log('contacts');
 
-    //   loadMain(button.id, content, contacts, contactTitle);
-    }
-  });
-})
+            //   loadMain(button.id, content, contacts, contactTitle);
+        }
+    });
+});
 
 function changeButtonCover(oldTab, newTab) {
-    // let hide = document.getElementById(`cover${oldTab}`);
-    // let show = document.getElementById(`cover${newTab}`);
-    
-    button.classList.add('cs-active');
-    // show.classList.remove('cs-active');
-  
-    return newTab;
-  } 
+    let hide = document.getElementById(`${newTab}`);
+    let show = document.getElementById(`${oldTab}`);
 
+    hide.classList.add('cs-active');
+    show.classList.remove('cs-active');
+
+    return newTab;
+}
