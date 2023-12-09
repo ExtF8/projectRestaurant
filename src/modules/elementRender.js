7893cea92672a5db65f80f46d97eb47b9082b56f
extf8 <extfield8@gmail.com>
2023-12-09 17:32:47 +0200

    Moved to element renderer module

---
+++ src/modules/elementRender.js
@@ -0,0 +1,159 @@
+/**
+ * Creates a section HTML element with specified attribute and value
+ * @param {string} attributeName - Name of the attribute
+ * @param {string} attributeValue - Value for the attribute
+ * @returns {HTMLElement} - The created section element
+ */
+export default function createSection(attributeName, attributeValue) {
+    const section = document.createElement('section');
+    section.setAttribute(attributeName, attributeValue);
+    return section;
+}
+
+/**
+ * Creates a div HTML element with a specified attribute name and value
+ * @param {object} attributeName - Name of the attribute
+ * @param {string} attributeValue - Value for the attribute
+ * @returns {HTMLDivElement} - The created div element
+ */
+export default function createDiv(attributeName, attributeValue) {
+    const div = document.createElement('div');
+    div.setAttribute(attributeName, attributeValue);
+    return div;
+}
+
+/**
+ * Creates a picture element with provided attributes and sources
+ * @param {object} attributeName - Name of the attribute
+ * @param {string} attributeValue  - Value for the attribute
+ * @param {object} imageSources - Object containing picture sources for different screen sizes
+ * @returns {HTMLPictureElement} - The created picture element
+ */
+export default function createPicture(attributeName, attributeValue, imageSources) {
+    const picture = document.createElement('picture');
+    picture.setAttribute(attributeName, attributeValue);
+
+    Object.keys(imageSources).forEach((key) => {
+        if (key === 'img') {
+            const img = document.createElement('img');
+            setAttributes(img, imageSources[key]);
+            picture.appendChild(img);
+        } else {
+            const source = document.createElement('source');
+            setAttributes(source, imageSources[key]);
+            picture.appendChild(source);
+        }
+    });
+
+    return picture;
+}
+
+/**
+ * Creates a list (ul) element with provided attributes. The list items are created using a rendering export default function passed as an argument.
+ * @param {string} attributeName - Name of the attribute (e.g., 'class').
+ * @param {string} attributeValue - Value for the attribute (e.g., 'cs-stats').
+ * @param {object} data - Data to populate the list with. Expects an object where each property can be rendered as a list item.
+ * @param {Function} renderItem - export default function that takes an item of data and returns a list item (li) element.
+ * @returns {HTMLUListElement} - The created unordered list element.
+ */
+export default function createList(attributeName, attributeValue, data, renderItem) {
+    const list = document.createElement('ul');
+    list.setAttribute(attributeName, attributeValue);
+
+    for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+            list.appendChild(renderItem(data[key]));
+        }
+    }
+
+    return list;
+}
+
+/**
+ * Creates a list item (li) element using a provided rendering export default function for the item's content.
+ * @param {string} attributeName - Name of the attribute (e.g., 'class').
+ * @param {string} attributeValue - Value for the attribute (e.g., 'cs-stat').
+ * @param {object} item - Data item to be rendered inside the list item.
+ * @param {Function} renderContent - export default function that takes the item and returns the content to be placed inside the list item.
+ * @returns {HTMLLIElement} - The created list item.
+ */
+export default function createListItem(attributeName, attributeValue, item, renderContent) {
+    const listItem = document.createElement('li');
+    listItem.setAttribute(attributeName, attributeValue);
+
+    const content = renderContent(item);
+    listItem.appendChild(content);
+
+    return listItem;
+}
+
+/**
+ * Creates a span element with a specified class and text content
+ * @param {string} attributeName - Name of the attribute
+ * @param {string} attributeValue - Value for the attribute
+ * @param {string} text - Text content for the span
+ * @returns {HTMLElement} - The created span element
+ */
+export default function createSpan(attributeName, attributeValue, text) {
+    const span = document.createElement('span');
+    span.setAttribute(attributeName, attributeValue);
+    span.innerText = text;
+
+    return span;
+}
+
+/**
+ * Creates a topper element (span) with a specified class and text content.
+ * @param {string} attributeName - Name of the attribute (e.g., 'class').
+ * @param {string} attributeValue - Value for the attribute.
+ * @param {string} text - Text content for the topper.
+ * @returns {HTMLElement} - The created topper element.
+ */
+export default function createTopper(attributeName, attributeValue, text) {
+    const topper = document.createElement('span');
+    topper.setAttribute(attributeName, attributeValue);
+    topper.innerText = text;
+
+    return topper;
+}
+
+/**
+ * Creates a title element (h2) with a specified class and text content.
+ * @param {string} attributeName - Name of the attribute (e.g., 'class').
+ * @param {string} attributeValue - Value for the attribute.
+ * @param {string} text - Text content for the title.
+ * @returns {HTMLHeadingElement} - The created title element.
+ */
+export default function createTitle(attributeName, attributeValue, text) {
+    const title = document.createElement('h2');
+    title.setAttribute(attributeName, attributeValue);
+    title.textContent = text;
+
+    return title;
+}
+
+/**
+ * Creates a paragraph element (p) with a specified class and text content.
+ * @param {string} attributeName - Name of the attribute (e.g., 'class').
+ * @param {string} attributeValue - Value for the attribute.
+ * @param {string} text - Text content for the paragraph.
+ * @returns {HTMLParagraphElement} - The created paragraph element.
+ */
+export default function createParagraph(attributeName, attributeValue, text) {
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute(attributeName, attributeValue);
+    paragraph.textContent = text;
+
+    return paragraph;
+}
+
+/**
+ * Sets multiple attributes on a DOM element based on a provided object.
+ * @param {HTMLElement} element - The DOM element to set attributes on.
+ * @param {object} attributes - Object containing key-value pairs for attributes.
+ */
+export default function setAttributes(element, attributes) {
+    Object.keys(attributes).forEach((attr) => {
+        element.setAttribute(attr, attributes[attr]);
+    });
+}
