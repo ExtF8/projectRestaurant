31cb0196abde7d35d0b065a3e398f4f02f3acfa3
extf8 <extfield8@gmail.com>
2023-12-11 11:41:15 +0200

    Refactored menuPageLoader into separate functions

--- src/modules/menuPageLoader.js
+++ src/modules/menuPageLoader.js
@@ -1,3 +1,5 @@
+import { menuContents } from './menuContent.js';
+
 import {
     createSection,
     createDiv,
@@ -9,51 +11,54 @@ import {
     createParagraph,
 } from './elementRender.js';
 
-import { menuContents } from './menuContent.js';
+const MENU_PAGE_CONTENTS = menuContents[0];
 
 export default function menuPageLoader() {
     const mainContent = document.querySelector('#main-content');
-    const section = createSection('id', 'menu-1005');
 
+    const section = createSection('id', 'menu-1005');
     mainContent.appendChild(section);
 
-    const container = createDiv('class', 'cs-container');
+    const container = renderContainer();
     section.appendChild(container);
+}
 
-    const content = createDiv('class', 'cs-content');
+function renderContainer() {
+    const container = createDiv('class', 'cs-container');
+    const content = renderContent();
+    const cardGroupContent = renderCardGroup();
+    const reserveButton = createReserveButton();
+
     container.appendChild(content);
+    container.appendChild(cardGroupContent);
+    container.appendChild(reserveButton);
 
+    return container;
+}
+
+function renderContent() {
+    const content = createDiv('class', 'cs-content');
     const title = createTitle(
         'class',
         'cs-title',
-        menuContents[0].content.title
+        MENU_PAGE_CONTENTS.content.title
     );
     const paragraph = createParagraph(
         'class',
         'cs-text',
-        menuContents[0].content.paragraph
+        MENU_PAGE_CONTENTS.content.paragraph
     );
-
     content.appendChild(title);
     content.appendChild(paragraph);
 
-    const cardGroupContent = renderCardGroup();
-
-    container.appendChild(cardGroupContent);
-
-    const reserveButton = document.createElement('a');
-    reserveButton.innerText = 'Reserve Your Table'
-    reserveButton.setAttribute('href', '');
-    reserveButton.setAttribute('class', 'cs-button-solid');
-
-    container.appendChild(reserveButton);
+    return content;
 }
 
 function renderCardGroup() {
     const list = createList(
         'class',
         'cs-card-group',
-        menuContents[0].cardGroup,
+        MENU_PAGE_CONTENTS.cardGroup,
         (itemData) =>
             createListItem('class', 'cs-item', itemData, renderListItemContent)
     );
@@ -64,29 +69,48 @@ function renderCardGroup() {
 function renderListItemContent(itemData) {
     const fragment = document.createDocumentFragment();
     const picture = createPicture('class', 'cs-picture', itemData.picture);
-    picture.setAttribute('area-hidden', 'true')
+    const infoContainer = renderInfoContainer(itemData);
 
+    picture.setAttribute('area-hidden', 'true');
+    fragment.appendChild(picture);
+    fragment.appendChild(infoContainer);
+
+    return fragment;
+}
+
+function renderInfoContainer(itemData) {
     const info = createDiv('class', 'cs-info');
-
-    const h3 = document.createElement('h3');
-    info.appendChild(h3);
-
+    const headingContainer = renderHeadingContainer(itemData);
     const paragraph = createParagraph(
         'class',
         'cs-item-p',
         itemData.info.itemParagraph
     );
 
+    info.appendChild(headingContainer);
+    info.appendChild(paragraph);
+
+    return info;
+}
+
+function renderHeadingContainer(itemData) {
+    const headingContainer = document.createElement('h3');
     const textSpan = createSpan('class', 'cs-name', itemData.info.name);
     const numberSpan = createSpan('class', 'cs-price', itemData.info.price);
 
-    h3.setAttribute('class', 'cs-h3');
-    h3.appendChild(textSpan);
-    h3.appendChild(numberSpan);
-    info.appendChild(paragraph);
+    headingContainer.setAttribute('class', 'cs-h3');
+    headingContainer.appendChild(textSpan);
+    headingContainer.appendChild(numberSpan);
 
-    fragment.appendChild(picture);
-    fragment.appendChild(info);
+    return headingContainer;
+}
 
-    return fragment;
+function createReserveButton() {
+    const reserveButton = document.createElement('a');
+
+    reserveButton.innerText = 'Reserve Your Table';
+    reserveButton.setAttribute('href', '');
+    reserveButton.setAttribute('class', 'cs-button-solid');
+
+    return reserveButton;
 }
